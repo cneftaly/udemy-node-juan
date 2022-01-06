a53c7c53922bf18f48650ae5540b9daf83cd361d
Carlos Sosa <carlosneftaly@hotmail.com>
2022-01-06 17:15:54 -0600

    Avance hasta el 06-ene-22 17:15

--- uptask/index.js
+++ uptask/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const routes = require('./routes');
 const path = require('path');
+//const bodyParser = require('body-parser'); UPDATE:
 
 const app = express();
 
+app.use(express.static('public'));
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname,  './views'));
-
+app.use(express.urlencoded({extended: true})); // UPDATE:
 app.use('/', routes());
 
 app.listen(3000);
--- uptask/routes/index.js
+++ uptask/routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const proyectosController = require('../controllers/proyectosController');
 
-const proyectosController = require('../controllers/proyectosController')
 module.exports = function() {
   router.get('/', proyectosController.proyectosHome);
-
+  router.get('/nuevo-proyecto', proyectosController.formularioProyecto);
+  router.post('/nuevo-proyecto', proyectosController.nuevoProyecto);
+  
   return router;
-}
+};
