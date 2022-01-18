e09da5c1b649e37a5285f32e78036dceee129869
Carlos Sosa <carlosneftaly@hotmail.com>
2022-01-17 21:37:23 -0600

    Se agrego helper vardump

--- uptask/index.js
+++ uptask/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 const path = require('path');
 //const bodyParser = require('body-parser'); UPDATE: Ya no es necesaria, viene incluido con express
-
+const helpers = require('./helpers')
 const db = require('./config/db');
 
 require('./models/Proyectos');
@@ -16,6 +16,11 @@ const app = express();
 app.use(express.static('public'));
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname,  './views'));
+app.use((req, res, next) => {
+  res.locals.vardump = helpers.vardump;
+  next();
+});
+
 app.use(express.urlencoded({extended: true})); // UPDATE: Se cambia de body-parser a express
 app.use('/', routes());
 
